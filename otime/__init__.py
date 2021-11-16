5475d3844211b1ce91db41f2f382de7af6996813
Mark Ivchenko <marxygen@gmail.com>
2021-11-16 13:02:29 +0300

    Create initial setup

---
+++ otime/__init__.py
@@ -0,0 +1,2 @@
+from .o_timer import O_time
+from .enums import *
---
+++ otime/exceptions.py
@@ -0,0 +1,8 @@
+class OTimingException(BaseException):
+    ...
+
+
+class InvalidParametersError(OTimingException):
+    # PEP8 about exception names: https://www.python.org/dev/peps/pep-0008/#exception-names
+    ...
+
